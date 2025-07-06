dfbc317c1e42687f8690d4aaa7e57f71f8f6ee6f
Aldar-Efremov1_L <Aldar-Efremov1_L@russiabpost.ru>
2025-07-06 23:47:31 +0300

    пополнил проект тестами

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,35 @@
+import sys
+from pathlib import Path
+
+root_dir = Path(__file__).parent.parent  # tests/.. → корень проекта
+sys.path.append(str(root_dir))
+
 import requests
 import pytest
 import random
 from datetime import timedelta
-from src.api.constant import HEADERS, BASE_URL
+from src.api.constant import Par
 from src.data_models.models import BookingResponse, PatchBookingData
 from typing import Dict, Any
 from faker import Faker
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 faker = Faker()
 
 
 
 @pytest.fixture(scope="session")
 def auth_session():
-    """Создаёт сессию с авторизацией и возвращает объект сессии."""
+    """
+    Создаёт сессию с авторизацией и возвращает объект сессии.
+    """
     session = requests.Session()
-    session.headers.update(HEADERS)
+    session.headers.update(Par.HEADERS.value)
 
-    auth_response = session.post(f"{BASE_URL}/auth", json={"username": "admin", "password": "password123"})
+    auth_response = session.post(f"{Par.BASE_URL.value}/auth", json={"username": os.getenv('APP_USERNAME'), "password": os.getenv('PASSWORD')})
     assert auth_response.status_code == 200, "Ошибка авторизации, статус код не 200"
 
     token = auth_response.json().get("token")
@@ -28,49 +41,12 @@ def auth_session():
 @pytest.fixture()
 def rndm_booking_id():
     session = requests.Session()
-    response = session.get(f"{BASE_URL}/booking")
+    response = session.get(f"{Par.BASE_URL.value}/booking")
     assert response.status_code == 200, "Ошибка авторизации, статус код не 200"
 
     get_booking = response.json()
     id = random.choice(get_booking)["bookingid"]
     return id
-
-@pytest.fixture(scope="session")
-def booking_data():
-    # # Сначала генерируем checkin, затем вычисляем checkout
-    # checkin = faker.date_between(start_date='today', end_date='+1y')
-    # checkout = checkin + timedelta(days=random.randint(1, 30))
-    #
-    # # Базовый набор данных (обязательные поля)
-    # data: Dict[str, Any] = {
-    #     "firstname": faker.first_name(),
-    #     "lastname": faker.last_name(),
-    #     "totalprice": faker.random_int(min=100, max=10000),
-    #     "depositpaid": True,
-    #     "bookingdates": {
-    #         "checkin": checkin.strftime("%Y-%m-%d"),  # Преобразуем в строку
-    #         "checkout": checkout.strftime("%Y-%m-%d")  # Преобразуем в строку
-    #     }
-    # }
-    #
-    # # С вероятностью 50% добавляем additionalneeds
-    # if random.choice([True, False]):
-    #     data["additionalneeds"] = faker.word().capitalize()
-    #
-    # return data
-    return {
-        "firstname": faker.first_name(),
-        "lastname": faker.last_name(),
-        "totalprice": faker.random_int(min=100, max=10000),
-        "depositpaid": True,
-        "bookingdates": {
-            "checkin": "2024-04-05",
-            "checkout": "2024-04-08"
-        },
-        "additionalneeds": "Breakfast"
-    }
-
-
 
 @pytest.fixture(scope="session")
 def upd_booking_data() -> BookingResponse:
--- tests/test_booking.py
+++ tests/test_booking.py
@@ -1,19 +1,40 @@
-from tests.conftest import booking_data
-from src.scenarios.test_scenarios import TestScenarios
+import pytest
+
+from src.scenarios.negative_scenarios import NegativeScenarios
+from src.scenarios.test_scenarios import Scenarios
 from src.api.booking_client import BookingApiClient
+from src.data_models.models import Generators
+
+@pytest.fixture
+def booking_engine(auth_session):
+    client = BookingApiClient(auth_session)
+    return Scenarios(client)
+
+@pytest.fixture
+def negative_booking_engine(auth_session):
+    client = BookingApiClient(auth_session)
+    return NegativeScenarios(client)
+
 
 class TestBookings:
 
-    def test_working(self):
-        assert 1 + 1 == 2
+    def test_positive_booking_creation(self, booking_engine):
+        booking_engine.create_booking_and_immediately_delete(Generators.booking_data())
 
-    def test_booking_creation(self, auth_session, booking_data):
-        client = BookingApiClient(auth_session)
-        booking = TestScenarios(client)
+    def test_positive_get_bookings(self, booking_engine):
+        booking_engine.get_and_verify_bookings_exist()
 
-        booking.create_booking_and_immediately_delete(booking_data)
+    def test_positive_update_random_booking(self, booking_engine):
+        booking_engine.update_booking_and_verify_changes(Generators.update_booking_data())
 
-        # Assert-проверки
-        # assert isinstance(booking, BookingResponse)
-        print(booking)
+    def test_positive_delete_random_booking(self, booking_engine):
+        booking_engine.delete_existing_booking_and_verify()
 
+    def test_negative_fail_creation_booking(self, negative_booking_engine):
+        negative_booking_engine.create_booking_with_invalid_payload(Generators.booking_data())
+
+    def test_negative_get_removed_booking(self, negative_booking_engine):
+        negative_booking_engine.get_removed_booking()
+
+    def test_negative_without_token_update_booking(self, negative_booking_engine):
+        negative_booking_engine.update_without_token(Generators.update_booking_data())
